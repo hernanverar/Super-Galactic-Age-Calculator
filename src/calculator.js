d32be323ec636cda2c293ca56ab694349acbf546
hernan verar <hernanverar@gmail.com>
2023-04-14 12:05:50 -0700

    first pass good test

---
+++ src/calculator.js
@@ -0,0 +1,9 @@
+export default class Age {
+  constructor(earthAge, mercuryAge, venusAge, marsAge, jupiterAge,) {
+  this.earthAge = earthAge;
+  this.mercuryAge = mercuryAge;
+  this.venusAge = venusAge;
+  this.marsAge = marsAge;
+  this.jupiterAge = jupiterAge;
+  }
+}
