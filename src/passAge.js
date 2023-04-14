466b477ef5059b47f09affc6f7d16124ff1b1b7b
hernan verar <hernanverar@gmail.com>
2023-04-14 16:26:39 -0700

    add our second good test by adding new file called passAge.js to help us know the years in different planets

---
+++ src/passAge.js
@@ -0,0 +1,10 @@
+
+export default class PassAge {
+  constructor(earthAgeHavePass, mercuryAgeHavePass, venusAgeHavePass, marsAgeHavePass, jupiterAgeHavePass) {
+  this.earthAgeHavePass = earthAgeHavePass;
+  this.mercuryAgeHavePass = mercuryAgeHavePass;
+  this.venusAgeHavePass = venusAgeHavePass;
+  this.marsAgeHavePass = marsAgeHavePass;
+  this.jupiterAgeHavePass = jupiterAgeHavePass;
+  }
+}
