5e84ab4dd88d1240923ac91bdac27cb058751007
hernan verar <hernanverar@gmail.com>
2023-04-14 16:56:00 -0700

    add future.js file will let us know our years in the future on the different planets

---
+++ src/futureAge.js
@@ -0,0 +1,9 @@
+export default class futureAge {
+  constructor(earthUntil, mercuryUntil, venusUntil, marsUntil , jupiterUntil) {
+    this.earthUntil = earthUntil;
+    this.mercuryUntil = mercuryUntil;
+    this.venusUntil = venusUntil;
+    this.marsUntil = marsUntil;
+    this.jupiterUntil = jupiterUntil;
+  }
+}
